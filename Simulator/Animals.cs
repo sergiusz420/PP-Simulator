fde95dde209242e65d8b4ed30e371536232d407c
sergiusz <sergiusz.sobol7@gmail.com>
2025-01-08 22:50:34 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,10 +1,30 @@
 ﻿namespace Simulator;
 
-    public class Animals
+public class Animals
     {
-        public required string Description { get; init; }
-        public uint Size { get; set; } = 3;
+        private string _description = ":D";
+        
+        public string Description 
+        {
+            get => _description;
+            init
+            {
+                _description = value;
+                _description = _description.Trim();
+                if (_description.Length > 15)
+                {
+                    _description = _description.Substring(0, 15).TrimEnd();
+                }
+                if (_description.Length < 3)
+                {
+                    _description = _description.PadRight(3, '#');
+                }
 
+                _description = char.ToUpper(_description[0]) + _description.Substring(1);
+        }
+    }
+       
+        public uint Size { get; set; } = 3;
         public string Info => $"{Description} <{Size}>";
     }
 
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,23 +1,60 @@
 ﻿namespace Simulator;
 
-    public class Creature
+public class Creature
 {
-        public string Name { get; set; }
-        public int Level { get; set; }
+    private string _name = "Unknown";
+    private int _level = 1;
 
-        public Creature(string name, int level = 1)
+    public string Name
+    {
+        get => _name;
+        init
         {
-            Name = name;
-            Level = level;
+            _name = value;
+            _name = _name.Trim();
+            if (_name.Length > 25)
+            {
+                _name = _name.Substring(0, 25).TrimEnd();
+            }
+            if (_name.Length < 3)
+            {
+                _name = _name.PadRight(3, '#');
+            }
+          
+            _name = char.ToUpper(_name[0]) + _name.Substring(1);
         }
-        public Creature() { }
-
-        public void SayHi()
-        {
-            Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
-        }
-
-        public string Info => $"{Name}, [{Level}]";
-
     }
+
+    public int Level
+    {
+        get => _level;
+        set
+        {
+            _level = value;
+            _level = _level < 1 ? 1 : _level;
+            _level = _level > 10 ? 10 : _level;
+        }
+    }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+    public Creature() { }
+
+    public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
+
+    public string Info => $"{Name}, [{Level}]";
+
+    public void Upgrade()
+    {
+        if (Level < 10)
+        {
+            _level++;
+        }
+    }
+      
+}
+    
 

24e37bd4750065ca5d3ed13d766d794fc59391ed
sergiusz <sergiusz.sobol7@gmail.com>
2025-01-09 19:05:42 +0100

    Prostokąty

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -5,62 +5,37 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Starting Simulator!");
-            Lab4a();
-            Lab4b();
-            Creature c = new Elf("Elandor", 5, 3);
-            Console.WriteLine(c);  // ELF: Elandor [5]
+            Lab5a();
         }
 
-        static void Lab4a()
+        private static void Lab5a()
         {
-            Console.WriteLine("HUNT TEST\n");
-            var o = new Orc() { Name = "Gorbag", Rage = 7 };
-            o.SayHi();
-            for (int i = 0; i < 10; i++)
+            var rect1 = new Rectangle(3, 5, 5, 3);
+            var rect2 = new Rectangle(new Point(5, 14), new Point(4, 12));
+
+            Console.WriteLine(rect1);
+            Console.WriteLine(rect2);
+
+            try
             {
-                o.Hunt();
-                o.SayHi();
+                var rect3 = new Rectangle(3, 3, 3, 5);
+                var rect4 = new Rectangle(new Point(5, 16), new Point(8, 18));
+
+                Console.WriteLine(rect3);
+                Console.WriteLine(rect4);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine($"Exception caught: {ex.Message}");
             }
 
-            Console.WriteLine("\nSING TEST\n");
-            var e = new Elf("Legolas", agility: 2);
-            e.SayHi();
-            for (int i = 0; i < 10; i++)
-            {
-                e.Sing();
-                e.SayHi();
-            }
+            var rectpoint = new Rectangle(1, 5, 5, 1);
+            var insidePoint = new Point(2, 2);
+            var outsidePoint = new Point(10, 1);
 
-            Console.WriteLine("\nPOWER TEST\n");
-            Creature[] creatures = {
-        o,
-        e,
-        new Orc("Morgash", 3, 8),
-        new Elf("Elandor", 5, 3)
-    };
-            foreach (Creature creature in creatures)
-            {
-                Console.WriteLine($"{creature.Name,-15}: {creature.Power}");
-            }
-        }
-
-        static void Lab4b()
-        {
-            object[] myObjects = {
-        new Animals() { Description = "dogs"},
-        new Birds { Description = "  eagles ", Size = 10 },
-        new Elf("e", 15, -3),
-        new Orc("morgash", 6, 4)
-    };
-            Console.WriteLine("\nMy objects:");
-            foreach (var o in myObjects) Console.WriteLine(o);
-            /*
-                My objects:
-                ANIMALS: Dogs <3>
-                BIRDS: Eagles (fly+) <10>
-                ELF: E## [10][0]
-                ORC: Morgash [6][4]
-            */
+            Console.WriteLine($"Rectangle: {rectpoint}");
+            Console.WriteLine($"Contains {insidePoint}: {rectpoint.Contains(insidePoint)}");
+            Console.WriteLine($"Contains {outsidePoint}: {rectpoint.Contains(outsidePoint)}");
         }
     }
 }
